9fe345e213dd6451d83eda0814dc4ce9fb969c9f
Sufyaan Tanveer <44135521+Sufyaan-T@users.noreply.github.com>
2024-02-03 06:02:00 +0000

    fix

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const video = document.getElementById('myVideo');
 const playButton = document.getElementById('playButton');
 
 playButton.addEventListener('click', () => {
-  if (video.paused) {
-    video.style.opacity = '1';
-    video.play();
-    playButton.style.opacity = '0';
-  }
+  video.style.opacity = '1'; 
+  video.play(); 
+  playButton.style.opacity = '0'; 
 });
